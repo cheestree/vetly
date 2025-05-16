4dd481f6d6d3cf144ea16eecdf298e8d394faca3
Daniel <101189638+cheestree@users.noreply.github.com>
2025-05-16 21:23:55 +0100

    Dockerfile fix? Maybe?

--- src/server/src/main/kotlin/com/cheestree/vetly/config/FirebaseConfig.kt
+++ src/server/src/main/kotlin/com/cheestree/vetly/config/FirebaseConfig.kt
@@ -1,7 +1,10 @@
 package com.cheestree.vetly.config
 
+import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import jakarta.annotation.PostConstruct
+import java.io.FileInputStream
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
@@ -10,8 +13,12 @@ import org.springframework.context.annotation.Profile
 class ProdFirebaseConfig {
     @PostConstruct
     fun init() {
+        val path = System.getenv("FIREBASE_CREDENTIALS_PATH")
+        val firebaseOptions = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(FileInputStream(path)))
+            .build()
         if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp()
+            FirebaseApp.initializeApp(firebaseOptions)
             println("Firebase initialized manually.")
         } else {
             println("Firebase already initialized.")
