1a39e491fe39343ca7624b087d1d042c96849923
Daniel <101189638+cheestree@users.noreply.github.com>
2025-10-29 00:18:47 +0000

    Modified Redis to not run on tests

--- src/server/src/main/kotlin/com/cheestree/vetly/config/RedisConfig.kt
+++ src/server/src/main/kotlin/com/cheestree/vetly/config/RedisConfig.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
@@ -16,6 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
 @Configuration
+@Profile("!test")
 class RedisConfig {
     @Bean
     fun redisConnectionFactory() = LettuceConnectionFactory()
